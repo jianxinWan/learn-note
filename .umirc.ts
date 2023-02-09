a0f0770eaf4a250df556d9feb6c4dc89af6b2c55
wanjianxin <wanjianxin@meituan.com>
2023-02-09 18:29:24 +0800

    feat: build doc

--- .umirc.ts
+++ .umirc.ts
@@ -4,5 +4,5 @@ export default defineConfig({
   title: 'origin',
   favicon: 'https://p0.meituan.net/travelcube/a70da08acbbeea6ce5f85ad7c8530411322973.png',
   logo: 'https://p0.meituan.net/travelcube/a70da08acbbeea6ce5f85ad7c8530411322973.png',
-  outputPath: 'docs-dist',
+  outputPath: 'build',
 });
