0341a6fe43520177d41f3a6b17e21bc2b9accebf
wanjianxin <wanjianxin@meituan.com>
2023-02-09 19:06:51 +0800

    feat: 修改base

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'origin',
+  base: 'base',
   favicon: 'https://p0.meituan.net/travelcube/a70da08acbbeea6ce5f85ad7c8530411322973.png',
   logo: 'https://p0.meituan.net/travelcube/a70da08acbbeea6ce5f85ad7c8530411322973.png',
-  outputPath: 'build',
+  outputPath: 'docs',
 });
